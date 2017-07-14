a7136ec098ccd77183de321d7cfc18d6358d370c
Brittany Kerr <kerrbrittany9@gmail.com>
2017-07-14 08:56:42 -0700

    write function to set stylist name

--- src/Stylist.php
+++ src/Stylist.php
@@ -12,6 +12,11 @@
             $this->id = $id;
         }
 
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
         function getName()
         {
             return $this->name;
