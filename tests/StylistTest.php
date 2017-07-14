c83e46255d2cc0ff4424b334266793f20e1a9a59
Brittany Kerr <kerrbrittany9@gmail.com>
2017-07-14 08:30:41 -0700

    add basic framework for stylist test

---
+++ tests/StylistTest.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+    require_once "src/Stylist.php";
+
+
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+
+    }
+?>
